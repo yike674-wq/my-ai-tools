0e2a3d94b6cb56e10e6460c6e0f36484551cc992
yike674-wq <yike674@gmail.com>
2026-02-10 11:29:30 -0600

    Add files via upload

--- web_app.py
+++ web_app.py
@@ -20,7 +20,7 @@ st.markdown("""
     .main { background-color: #f8f9fa; }
     .stMetric { background-color: #ffffff; padding: 15px; border-radius: 10px; box-shadow: 0 2px 4px rgba(0,0,0,0.05); }
     </style>
-    """, unsafe_allow_index=True)
+    """, unsafe_allow_html=True) # 改成 html 就行了！
 
 # --- 2. 初始化记忆 ---
 if "messages" not in st.session_state:
